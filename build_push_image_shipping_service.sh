60c444ccc5d6bb3566f1916eb1359c9d4ce4c520
hafizaliyudin <hafizhaliyuddin12@gmail.com>
2023-10-27 21:35:58 +0700

    add new file

---
+++ build_push_image_shipping_service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build Docker image untuk backend
+docker build -t a433-microservices/shipping-service:v1 .
+
+# Login ke GitHub Package
+echo $PASSWORD_GITHUB | docker login docker.pkg.github.com -u hafizaliyudin --password-stdin
+
+# Push image ke GitHub Package
+docker tag a433-microservices/shipping-service:v1 docker.pkg.github.com/hafizaliyudin/a433-microservices/shipping-service:v1
+docker push docker.pkg.github.com/hafizaliyudin/a433-microservices/shipping-service:v1
